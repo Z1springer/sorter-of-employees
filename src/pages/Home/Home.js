3e449d8a2d46ef9466e79c48be90e7873801805c
Zach <zdspringer99@gmail.com>
2021-02-16 20:03:34 -0800

    separated pages for organization purposes

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <div className="jumbotron">
+        <h1 className="display-4">Greetings and Well-Met!</h1>
+        <p className="lead">
+          You are here to sort and categorize some imaginary folks right? Well
+          get to it!
+        </p>
+        <hr className="my-4"></hr>
+        <p>
+          Meaning that when you wish you may go to the search page to search for
+          specific imaginary people, or you can sort the whole lot of 'em on the
+          sort page!
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
