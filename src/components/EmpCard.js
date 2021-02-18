6e31fc07cae4ccfb4c893e422d9b8a209ae07d16
Zach <zdspringer99@gmail.com>
2021-02-17 21:19:09 -0800

    properly referenced the information needed from the api

--- src/components/EmpCard.js
+++ src/components/EmpCard.js
@@ -1,32 +1,36 @@
 import React from "react";
+import "./EmpCard.css";
 
 function EmpCard(props) {
   return (
     <div className="card">
       <div className="img-container">
-        <img alt={props.name} src={props.picture.large} />
+        <img
+          alt={props.employee.name.first}
+          src={props.employee.picture.large}
+        />
       </div>
       <div className="content">
         <ul>
           <li>
-            <strong>Name:</strong>
-            {props.name}
+            <strong>Name: </strong>
+            {props.employee.name.first} {props.employee.name.last}
           </li>
           <li>
-            <strong>Email:</strong>
-            {props.email}
+            <strong>Email: </strong>
+            {props.employee.email}
           </li>
           <li>
-            <strong>Age:</strong>
-            {props.dob.age}
+            <strong>Age: </strong>
+            {props.employee.dob.age}
           </li>
           <li>
-            <strong>Location:</strong>
-            {(props.location.street, props.location.city)}
+            <strong>Home City: </strong>
+            {props.employee.location.city}
           </li>
           <li>
-            <strong>Phone Number:</strong>
-            {props.phone}
+            <strong>Phone Number: </strong>
+            {props.employee.phone}
           </li>
         </ul>
       </div>
